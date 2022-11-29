3874ebb3c513179041489a270a097ea56d43e6da
Zhuxiaoying2002 <115535180+Zhuxiaoying2002@users.noreply.github.com>
2022-11-30 01:12:50 +0800

    用户更新功能

---
+++ project6/src/cn/edu/swu/conf/CharsetFilter.java
@@ -0,0 +1,18 @@
+package cn.edu.swu.conf;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public abstract class CharsetFilter extends HttpFilter {
+
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+}
